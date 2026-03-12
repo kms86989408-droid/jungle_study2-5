0c84c5d471c40e07337a88560d00ab6d803fe05a
고명석 <kms86989408@gmail.com>
2026-03-12 17:42:00 +0900

    solve : baekjun 2562

--- week2/problem-solving/난이도하_파이썬문법_최댓값_브론즈3.py
+++ week2/problem-solving/난이도하_파이썬문법_최댓값_브론즈3.py
@@ -1,2 +1,17 @@
 # 파이썬 문법 - 최댓값 (백준 브론즈3)
 # 문제 링크: https://www.acmicpc.net/problem/2562
+
+nums = []
+for i in range(9):
+    nums.append(int(input()))
+
+max_nums = nums[0]
+max_index = 0
+
+for j in range(1, 9):
+    if nums[j] > max_nums :
+        max_nums = nums[j]
+        max_index = j
+
+print(max_nums)
+print(max_index+1)
