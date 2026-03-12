ffc5b4762743efb247e7d5990c9138481281b8bb
고명석 <kms86989408@gmail.com>
2026-03-12 22:17:30 +0900

    solve : baekjoon 4344

--- week2/problem-solving/난이도하_배열_평균은넘겠지_브론즈1.py
+++ week2/problem-solving/난이도하_배열_평균은넘겠지_브론즈1.py
@@ -1,2 +1,19 @@
 # 배열 - 평균은 넘겠지 (백준 브론즈1)
 # 문제 링크: https://www.acmicpc.net/problem/4344
+
+n = int(input())
+
+for i in range(n):
+    nums = list(map(int, input().split()))
+    students = nums[0]
+    scores = nums[1:]
+
+    avg = sum(scores)/students
+    count = 0
+
+    for score in scores:
+        if score > avg:
+             count += 1
+
+    ratio = (count / students) * 100
+    print(f"{ratio:.3f}%")
