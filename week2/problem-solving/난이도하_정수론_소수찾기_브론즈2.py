4fa4379df172be2f7cb03aaa5d726177a6f0cf98
고명석 <kms86989408@gmail.com>
2026-03-12 19:07:55 +0900

    solve : baekjun_1978

--- week2/problem-solving/난이도하_정수론_소수찾기_브론즈2.py
+++ week2/problem-solving/난이도하_정수론_소수찾기_브론즈2.py
@@ -1,2 +1,20 @@
 # 정수론 - 소수 찾기 (백준 브론즈2)
 # 문제 링크: https://www.acmicpc.net/problem/1978
+
+n = int(input())
+nums = list(map(int, input().split()))
+count = 0 
+
+for i in range(n):
+    if nums[i] < 2 :
+        continue
+
+    is_prime = True
+    for j in range(2, nums[i]):
+        if nums[i] % j == 0:
+            is_prime = False
+            break
+    if is_prime:
+        count += 1
+
+print(count)
