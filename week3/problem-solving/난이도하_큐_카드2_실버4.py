625d1adbfa3e326a29809e952dfa66f16102fa47
고명석 <kms86989408@gmail.com>
2026-03-16 13:54:51 +0900

    solve : baekjoon 2164

--- week3/problem-solving/난이도하_큐_카드2_실버4.py
+++ week3/problem-solving/난이도하_큐_카드2_실버4.py
@@ -1,2 +1,16 @@
 # 큐 - 카드2 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/2164
+from collections import deque
+
+n = int(input())
+card_stack = deque(range(1, n+1))
+card_arry = []
+    
+while True:
+    if len(card_stack) == 1:
+        print(card_stack[0])
+        break
+    else :
+        card_arry.append(card_stack.popleft())
+        card_stack.append(card_stack.popleft())
+        
