b720ccc083b835cbebcc295da2b6627f98a1fae1
고명석 <kms86989408@gmail.com>
2026-03-17 16:57:33 +0900

    solve : baekjoon 1406

--- week3/problem-solving/난이도하_링크드리스트_에디터_실버2.py
+++ week3/problem-solving/난이도하_링크드리스트_에디터_실버2.py
@@ -1,2 +1,72 @@
 # 링크드리스트 - 에디터 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/1406
+import sys
+
+# s = sys.stdin.readline().strip()
+# m = int(sys.stdin.readline())
+
+# left = list(s)    
+# right = []
+
+# for i in range(m):
+#     cmd = sys.stdin.readline().split()
+
+#     if cmd[0] == "L":
+#         if len(left) != 0 :
+#             right.append(left.pop())
+#     elif cmd[0] == "D":
+#         if len(right) != 0 :    
+#             left.append(right.pop())    
+#     elif cmd[0] == "B":
+#         if len(left) != 0 :
+#             left.pop()
+#     elif cmd[0] == "P":
+#         left.append(cmd[1])
+
+# print("".join(left+right[::-1]))
+input = sys.stdin.readline
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.prev = None
+        self.next = None
+
+def insert(cursor, data):
+    node = Node(data)
+    node.prev = cursor.prev
+    node.next = cursor
+    cursor.prev.next = node
+    cursor.prev = node        
+        
+s = input().rstrip()
+m = int(input())
+
+head = Node(None) #시작더미
+tail = Node(None) #끝 더미
+head.next = tail
+tail.prev = head
+cursor = tail
+
+for c in s:
+    insert(cursor, c)
+        
+for i in range(m):
+    cmd = input().split()
+    if cmd[0] == "L":
+        if cursor.prev != head:
+            cursor = cursor.prev
+    elif cmd[0] == "D":
+            if cursor != tail:
+                cursor = cursor.next
+    elif cmd[0] == "B":
+        if cursor.prev != head:
+            cursor.prev.prev.next = cursor
+            cursor.prev = cursor.prev.prev
+    elif cmd[0] == "P":
+        insert(cursor, cmd[1])
+node = head.next
+result = []
+while node != tail:
+    result.append(node.data)
+    node = node.next
+sys.stdout.write("".join(result))
