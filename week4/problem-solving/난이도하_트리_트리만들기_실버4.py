bf2e974fe6640a4cbb392dac6fa77a5f99885ae1
MyeongSeokKo <kms86989408@gmail.com>
2026-03-21 11:19:48 +0900

    solve : BOJ 14244

--- week4/problem-solving/난이도하_트리_트리만들기_실버4.py
+++ week4/problem-solving/난이도하_트리_트리만들기_실버4.py
@@ -1,2 +1,11 @@
 # 트리 - 트리 만들기 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/14244
+
+n, m = map(int, input().split())
+
+cur  = 0 
+
+for nxt in range(1, n):
+    print(cur,nxt)
+    if cur < n - m:
+        cur += 1
