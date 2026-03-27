e0f8305e98c3f48e5c8e3d2dcb3c481fc7335d37
MyeongSeokKo <kms86989408@gmail.com>
2026-03-27 19:20:01 +0900

    solve:BOJ2748

--- week5/problem-solving/난이도하_DP_피보나치수2_브론즈1.py
+++ week5/problem-solving/난이도하_DP_피보나치수2_브론즈1.py
@@ -1,2 +1,18 @@
 # DP - 피보나치 수 2 (백준 브론즈 1)
 # 문제 링크: https://www.acmicpc.net/problem/2748
+
+n = int(input())
+memo = {}
+
+def fibo(n, memo):
+    if n <= 1:
+        return n
+
+    if n in memo:
+        return memo[n]
+    
+    memo[n] = fibo(n - 1, memo) + fibo(n -2, memo)
+
+    return memo[n]
+
+print(fibo(n, memo))
