9cac59d729f838947f6eaed0bd230645f32e077f
MyeongSeokKo <kms86989408@gmail.com>
2026-03-27 21:34:05 +0900

    solve:BOJ1904

--- week5/problem-solving/난이도하_DP_01타일_실버3.py
+++ week5/problem-solving/난이도하_DP_01타일_실버3.py
@@ -1,2 +1,21 @@
 # DP - 01타일 (백준 실버3)
 # 문제 링크: https://www.acmicpc.net/problem/1904
+
+n = int(input())
+
+def fibo():
+    if n == 1:
+        return 1
+
+    dp = [0] * (n + 1)
+
+    dp[0] = 1
+    dp[1] = 1
+
+    for i in range(2, n+1):
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 15746
+
+    return dp[n]
+
+print(fibo())
+ 
