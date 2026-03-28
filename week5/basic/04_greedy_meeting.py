f61e08cccc205428a59963ca5d7a08c5be588bb8
MyeongSeokKo <kms86989408@gmail.com>
2026-03-28 11:07:03 +0900

    solve:basic_그리디 - 회의실 배정

--- week5/basic/04_greedy_meeting.py
+++ week5/basic/04_greedy_meeting.py
@@ -33,19 +33,25 @@ def select_meetings(meetings):
         (배정된 회의 개수, 선택된 회의 리스트)
     """
     # TODO: 회의가 없으면 0 반환
-    pass
+    if not meetings:
+        return 0, []
     
     # TODO: 종료 시간 기준으로 정렬
-    pass
-    
+    meetings.sort(key=lambda meeting: meeting[1])
     selected = []
-    
+
     # TODO: 첫 번째 회의 선택
-    pass
-    
+    selected.append(meetings[0])
+
     # TODO: 나머지 회의들 확인
     ## 이전 회의가 끝난 후 시작하는 회의만 선택
-    pass
+    last_end = meetings[0][1]
+
+    for i in range(1, len(meetings)):
+        s, e = meetings[i]
+        if s >= last_end:
+            selected.append(meetings[i])
+            last_end = e
     
     return len(selected), selected
 
