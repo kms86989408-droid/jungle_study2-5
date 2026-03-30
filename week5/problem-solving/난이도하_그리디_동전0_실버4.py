312783772b2d03f7c6d895a08d4e05615031dfb4
MyeongSeokKo <kms86989408@gmail.com>
2026-03-30 10:53:39 +0900

    solve:BOJ11047_greedy동전0

--- week5/problem-solving/난이도하_그리디_동전0_실버4.py
+++ week5/problem-solving/난이도하_그리디_동전0_실버4.py
@@ -1,2 +1,23 @@
 # 그리디 - 동전 0 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/11047
+
+n = list(map(int, input().split()))
+m = n[0]
+price = n[1]
+coins = []
+total_coin = 0
+
+for i in range(m):
+    k = int(input())
+    coins.append(k)
+
+coins.reverse()
+
+for coin in coins:
+    count = price // coin
+    price = price % coin
+
+    if count > 0:
+        total_coin += count
+
+print(total_coin)
